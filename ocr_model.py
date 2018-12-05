0971b65b05d4ca211cdb4f36c2df15eecea9a949
harshit sinha <harshit886@outlook.com>
2018-12-05 17:35:48 +0530

    Update ocr_model.py

--- ocr_model.py
+++ ocr_model.py
@@ -11,7 +11,7 @@ import datetime
 # import editdistance
 import numpy as np
 from scipy import ndimage
-import pylab
+# import pylab
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from keras import backend as K
